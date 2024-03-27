4525503a3f77828e9a11b223450ebf493552a43c
Nguyen duy hung <duyhungfa102@gmail.com>
2024-03-27 21:35:39 +0700

    update env : add email support

--- src/api/mobile/auth.ts
+++ src/api/mobile/auth.ts
@@ -133,7 +133,7 @@ router.post("/send-code-reset-pass", async_handle(async (req, res) => {
             }
         }).exec(),
         sendMailService.sendMailWithMailjet({
-            fromEmail: "hungnguyen13.t@gmail.com",
+            fromEmail: process.env.EMAIL_SUPPORT || "hungnguyen13.t@gmail.com",
             toEmail: user.email,
             content: `<div style="color: black;">
                 <h2>Đây là mã code của bạn : </h2>
